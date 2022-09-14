421a7c9dc53a320ab02361dfedc1ba96fdcf78c8
Luca Plaster <lucaplaster@gmail.com>
2022-09-14 00:15:33 -0300

    manipulating DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,34 @@
 class CalcController{
      
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate; 
+        this.initialize(); 
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display"); //Seleciona elemntos por meio de seletores CSS3
+        let dataEl = document.querySelector("#data");
+        let horaEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567"; //Manipula o HTML pelo DOM
+        dataEl.innerHTML = "01/05";
+        horaEl.innerHTML = "00:00";
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._currentDate; 
+    }
+
+    set dataAtual(valor){
+        this._currentDate = valor;
+    }
 }
