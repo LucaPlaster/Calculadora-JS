f463fb94a8c678a9334a0160f424f813a05df536
Luca Plaster <lucaplaster@gmail.com>
2022-09-15 12:26:50 -0300

    Switch and NaN/lenght operations

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,81 +1,250 @@
-class CalcController{
-     
+class CalcController {
+
     constructor(){
+
+        this._operation = [];
         this._locale = 'pt-BR';
-        this._displayCalcEl = document.querySelector("#display"); //Seleciona elemntos por meio de seletores CSS3
+        this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
-        this._currentDate; 
-        this.initialize(); 
+        this._currentDate;
+        this.initialize();
         this.initButtonsEvents();
+
     }
 
     initialize(){
-        this.setDisplayDateTime(); //Não ter carência de 1s para aparecer na tela
 
-        setInterval(() =>{
+        this.setDisplayDateTime()
+
+        setInterval(()=>{
+
             this.setDisplayDateTime();
-        }, 1000)    //Função executada em um intervalo de tempo em ms
+
+        }, 1000);
+
     }
 
     addEventListenerAll(element, events, fn){
+
         events.split(' ').forEach(event => {
+
             element.addEventListener(event, fn, false);
+
         })
+    
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation(){
+
+        return this._operation[this._operation.length-1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length-1] = value;
+
+    }
+
+    isOperator(value){
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+
+    }
+
+    addOperation(value){
+
+        console.log('A', isNaN(this.getLastOperation()));
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if (isNaN(value)){
+
+                console.log(value);
+
+            } else {
+
+                this._operation.push(value);
+
+            }
+
+        } else {
+
+            let newValue = this.getLastOperation().toString() + value.toString();
+
+            this.setLastOperation(parseInt(newValue));
+
+        }
+
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+        
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                this.addOperation('+');
+                break;
+
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+
+            case 'divisao':
+                this.addOperation('/');
+                break;
+
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+
+            case 'porcento':
+                this.addOperation('%');
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case 'ponto':
+                this.addOperation('.');
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+
     }
 
     initButtonsEvents(){
-        let buttons = document.querySelectorAll("#buttons >  g, #parts > g"); //seleciona toda classe button com descendencia em g
 
-        buttons.forEach((btn, index)=>{ //forEach percorre todo id button
-            this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace("btn-",""));
-            });
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-","");
+
+                this.execBtn(textBtn);
+
+            })
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
                 btn.style.cursor = "pointer";
+
             })
+
         })
+
     }
 
     setDisplayDateTime(){
+
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "short",
             year: "numeric"
         });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
     }
 
     get displayTime(){
+
         return this._timeEl.innerHTML;
+
     }
 
     set displayTime(value){
-        this._timeEl.innerHTML = value;
+
+        return this._timeEl.innerHTML = value;
+
     }
 
     get displayDate(){
+
         return this._dateEl.innerHTML;
+
     }
 
     set displayDate(value){
-        this._dateEl.innerHTML = value;
+
+        return this._dateEl.innerHTML = value;
+
     }
 
     get displayCalc(){
+
         return this._displayCalcEl.innerHTML;
+
     }
 
     set displayCalc(value){
+
         this._displayCalcEl.innerHTML = value;
+
     }
 
     get currentDate(){
+
         return new Date();
+
     }
 
     set currentDate(value){
+
         this._currentDate = value;
+
     }
+
 }
