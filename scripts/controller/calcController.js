11a053e9c763414346dea394821fc9803a381958
Luca Plaster <lucaplaster@gmail.com>
2022-09-17 10:23:11 -0300

    Ctrl + c and Ctrl + v

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -17,6 +17,36 @@ class CalcController {
 
     }
 
+    pasteFromClipboard(){
+
+        document.addEventListener('paste', e =>{
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+            console.log(text);
+
+        })
+
+    }
+
+    copyToClipboard(){
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+
+    }
+
     initialize(){
 
         this.setDisplayDateTime()
@@ -28,6 +58,7 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
 
     }
 
@@ -73,6 +104,10 @@ class CalcController {
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if(e.ctrlKey) this.copyToClipboard();
                     break;
     
             }
