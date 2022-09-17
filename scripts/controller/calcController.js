b5496a36faa07bb73408f78a0a7d07f9d6d0ec9d
Luca Plaster <lucaplaster@gmail.com>
2022-09-17 10:51:01 -0300

    error handling

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -204,9 +204,13 @@ class CalcController {
 
     getResult(){
 
-
-
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        } catch(e){
+            setTimeout(() => {
+                this.setError();
+            }, 1)
+        }
 
     }
 
@@ -480,6 +484,11 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if (value.toString().length > 10){
+            this.setError();
+            return false;
+        }
 
         this._displayCalcEl.innerHTML = value;
 
